911835efed4069f0760794be29fe45228d114709
Pavlo Sheshenia <sheshenia@outlook.com>
2022-05-26 15:53:04 +0300

    started schema migration from MySQL to PostgreSQL

---
+++ storage/postgresql/queue.go
@@ -0,0 +1,131 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/micromdm/nanomdm/mdm"
+)
+
+func enqueue(ctx context.Context, tx *sql.Tx, ids []string, cmd *mdm.Command) error {
+	if len(ids) < 1 {
+		return errors.New("no id(s) supplied to queue command to")
+	}
+	_, err := tx.ExecContext(
+		ctx,
+		`INSERT INTO commands (command_uuid, request_type, command) VALUES (?, ?, ?);`,
+		cmd.CommandUUID, cmd.Command.RequestType, cmd.Raw,
+	)
+	if err != nil {
+		return err
+	}
+	query := `INSERT INTO enrollment_queue (id, command_uuid) VALUES (?, ?)`
+	query += strings.Repeat(", (?, ?)", len(ids)-1)
+	args := make([]interface{}, len(ids)*2)
+	for i, id := range ids {
+		args[i*2] = id
+		args[i*2+1] = cmd.CommandUUID
+	}
+	_, err = tx.ExecContext(ctx, query+";", args...)
+	return err
+}
+
+func (m *MySQLStorage) EnqueueCommand(ctx context.Context, ids []string, cmd *mdm.Command) (map[string]error, error) {
+	tx, err := m.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	if err = enqueue(ctx, tx, ids, cmd); err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return nil, fmt.Errorf("rollback error: %w; while trying to handle error: %v", rbErr, err)
+		}
+		return nil, err
+	}
+	return nil, tx.Commit()
+}
+
+func (s *MySQLStorage) StoreCommandReport(r *mdm.Request, result *mdm.CommandResults) error {
+	if err := s.updateLastSeen(r); err != nil {
+		return err
+	}
+	if result.Status == "Idle" {
+		return nil
+	}
+	notNowConstants := "NULL, 0"
+	notNowBumpTallySQL := ""
+	// note that due to the ON DUPLICATE KEY we don't UPDATE the
+	// not_now_at field. thus it will only represent the first NotNow.
+	if result.Status == "NotNow" {
+		notNowConstants = "CURRENT_TIMESTAMP, 1"
+		notNowBumpTallySQL = `, command_results.not_now_tally = command_results.not_now_tally + 1`
+	}
+	_, err := s.db.ExecContext(
+		r.Context, `
+INSERT INTO command_results
+    (id, command_uuid, status, result, not_now_at, not_now_tally)
+VALUES
+    (?, ?, ?, ?, `+notNowConstants+`) AS new
+ON DUPLICATE KEY
+UPDATE
+    status = new.status,
+    result = new.result`+notNowBumpTallySQL+`;`,
+		r.ID,
+		result.CommandUUID,
+		result.Status,
+		result.Raw,
+	)
+	return err
+}
+
+func (s *MySQLStorage) RetrieveNextCommand(r *mdm.Request, skipNotNow bool) (*mdm.Command, error) {
+	statusWhere := "status IS NULL"
+	if !skipNotNow {
+		statusWhere = `(` + statusWhere + ` OR status = 'NotNow')`
+	}
+	command := new(mdm.Command)
+	err := s.db.QueryRowContext(
+		r.Context,
+		`SELECT command_uuid, request_type, command FROM view_queue WHERE id = ? AND active = 1 AND `+statusWhere+` LIMIT 1;`,
+		r.ID,
+	).Scan(&command.CommandUUID, &command.Command.RequestType, &command.Raw)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return command, nil
+}
+
+func (s *MySQLStorage) ClearQueue(r *mdm.Request) error {
+	if r.ParentID != "" {
+		return errors.New("can only clear a device channel queue")
+	}
+	// Because we're joining on and WHERE-ing by the enrollments table
+	// this will clear (mark inactive) the queue of not only this
+	// device ID, but all user-channel enrollments with a 'parent' ID of
+	// this device, too.
+	_, err := s.db.ExecContext(
+		r.Context,
+		`
+UPDATE
+    enrollment_queue AS q
+	INNER JOIN enrollments AS e
+	    ON q.id = e.id
+    INNER JOIN commands AS c
+        ON q.command_uuid = c.command_uuid
+    LEFT JOIN command_results r
+        ON r.command_uuid = q.command_uuid AND r.id = q.id
+SET
+    q.active = 0
+WHERE
+    e.device_id = ? AND
+    active = 1 AND
+    (r.status IS NULL OR r.status = 'NotNow');`,
+		r.ID,
+	)
+	return err
+}
--- storage/postgresql/schema.sql
+++ storage/postgresql/schema.sql
@@ -1,35 +1,37 @@
-CREATE TABLE devices (
-    id VARCHAR(255) NOT NULL,
+CREATE TABLE devices
+(
+    id                  VARCHAR(255) NOT NULL,
 
-    identity_cert TEXT NULL,
+    identity_cert       TEXT NULL,
 
-    serial_number VARCHAR(127) NULL,
+    serial_number       VARCHAR(127) NULL,
 
     -- If the (iOS, iPadOS) device sent an UnlockToken in the TokenUpdate
     -- TODO: Consider using a TEXT field and encoding the binary
-    unlock_token    MEDIUMBLOB NULL,
-    unlock_token_at TIMESTAMP  NULL,
+    unlock_token        TEXT NULL,
+    unlock_token_at     TIMESTAMP NULL,
 
     -- The last raw Authenticate for this device
-    authenticate    TEXT      NOT NULL,
-    authenticate_at TIMESTAMP NOT NULL,
+    authenticate        TEXT         NOT NULL,
+    authenticate_at     TIMESTAMP    NOT NULL,
     -- The last raw TokenUpdate for this device
-    token_update    TEXT      NULL,
-    token_update_at TIMESTAMP NULL,
+    token_update        TEXT NULL,
+    token_update_at     TIMESTAMP NULL,
 
-    bootstrap_token_b64 TEXT      NULL,
+    bootstrap_token_b64 TEXT NULL,
     bootstrap_token_at  TIMESTAMP NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- TODO  MySQL ON UPDATE CURRENT_TIMESTAMP to pgSQL trigger func
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id),
 
     CHECK (identity_cert IS NULL OR
-        SUBSTRING(identity_cert FROM 1 FOR 27) = '-----BEGIN CERTIFICATE-----'),
+           SUBSTRING(identity_cert FROM 1 FOR 27) = '-----BEGIN CERTIFICATE-----'),
 
-    CHECK (serial_number IS NULL OR serial_number != ''),
-    INDEX (serial_number),
+    CHECK (serial_number IS NULL OR serial_number != ''
+) ,
 
     CHECK (unlock_token IS NULL OR LENGTH(unlock_token) > 0),
 
@@ -38,35 +40,39 @@ CREATE TABLE devices (
 
     CHECK (bootstrap_token_b64 IS NULL OR bootstrap_token_b64 != '')
 );
+CREATE INDEX serial_number ON devices (serial_number);
 
+CREATE TABLE users
+(
+    id                          VARCHAR(255) NOT NULL,
+    device_id                   VARCHAR(255) NOT NULL,
 
-CREATE TABLE users (
-    id        VARCHAR(255) NOT NULL,
-    device_id VARCHAR(255) NOT NULL,
-
-    user_short_name VARCHAR(255) NULL,
-    user_long_name  VARCHAR(255) NULL,
+    user_short_name             VARCHAR(255) NULL,
+    user_long_name              VARCHAR(255) NULL,
 
     -- The last raw TokenUpdate for this user
-    token_update    TEXT      NULL,
-    token_update_at TIMESTAMP NULL,
+    token_update                TEXT NULL,
+    token_update_at             TIMESTAMP NULL,
 
     -- The last raw UserAuthenticate (and optional digest) for this user
-    user_authenticate           TEXT      NULL,
+    user_authenticate           TEXT NULL,
     user_authenticate_at        TIMESTAMP NULL,
-    user_authenticate_digest    TEXT      NULL,
+    user_authenticate_digest    TEXT NULL,
     user_authenticate_digest_at TIMESTAMP NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- TODO  MySQL ON UPDATE CURRENT_TIMESTAMP to pgSQL trigger func
+    updated_at                  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id, device_id),
+    UNIQUE (id),
 
     FOREIGN KEY (device_id)
         REFERENCES devices (id)
         ON DELETE CASCADE ON UPDATE CASCADE,
 
-    CHECK (user_short_name IS NULL OR user_short_name != ''),
+    CHECK (user_short_name IS NULL OR user_short_name != ''
+) ,
     CHECK (user_long_name  IS NULL OR user_long_name  != ''),
 
     CHECK (token_update IS NULL OR token_update != ''),
@@ -75,40 +81,42 @@ CREATE TABLE users (
     CHECK (user_authenticate_digest IS NULL OR user_authenticate_digest != '')
 );
 
-
 /* This table represents enrollments which are an amalgamation of
  * both device and user enrollments.
  */
-CREATE TABLE enrollments (
+CREATE TABLE enrollments
+(
     -- The enrollment ID of this enrollment
-    id        VARCHAR(255) NOT NULL,
+    id                 VARCHAR(255) NOT NULL,
     -- The "device" enrollment ID of this enrollment. This will be
     -- the same as the `id` field in the case of a "device" enrollment,
     -- or will be the "parent" enrollment for a "user" enrollment.
-    device_id VARCHAR(255) NOT NULL,
+    device_id          VARCHAR(255) NOT NULL,
     -- The "user" enrollment ID of this enrollment. This will be the
     -- same as the `id` field in the case of a "user" enrollment or
     -- NULL in the case of a device enrollment.
-    user_id   VARCHAR(255) NULL,
+    user_id            VARCHAR(255) NULL,
 
     -- Textual representation of the type of device enrollment.
-    type      VARCHAR(31) NOT NULL,
+    type               VARCHAR(31)  NOT NULL,
 
     -- The MDM APNs push trifecta.
-    topic      VARCHAR(255) NOT NULL,
-    push_magic VARCHAR(127) NOT NULL,
-    token_hex  VARCHAR(255) NOT NULL, -- TODO: Perhaps just CHAR(64)?
+    topic              VARCHAR(255) NOT NULL,
+    push_magic         VARCHAR(127) NOT NULL,
+    token_hex          VARCHAR(255) NOT NULL, -- TODO: Perhaps just CHAR(64)?
 
-    enabled            BOOLEAN NOT NULL DEFAULT 1,
-    token_update_tally INTEGER NOT NULL DEFAULT 1,
+    enabled            BOOLEAN      NOT NULL DEFAULT TRUE,
+    token_update_tally INTEGER      NOT NULL DEFAULT 1,
 
-    last_seen_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_seen_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at         TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+    -- TODO  MySQL ON UPDATE CURRENT_TIMESTAMP to pgSQL trigger func
+    updated_at         TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id),
-    CHECK (id != ''),
+    CHECK (id != ''
+) ,
 
     FOREIGN KEY (device_id)
         REFERENCES devices (id)
@@ -120,30 +128,32 @@ CREATE TABLE enrollments (
     UNIQUE (user_id),
 
     CHECK (type != ''),
-    INDEX (type),
 
     CHECK (topic != ''),
     CHECK (push_magic != ''),
     CHECK (token_hex != '')
 );
+CREATE INDEX idx_type ON enrollments (type);
 
 
 /* Commands stand alone. By themsevles they aren't associated with
  * a device, a result (response), etc. Joining other tables is required
  * for more context.
  */
-CREATE TABLE commands (
+CREATE TABLE commands
+(
     command_uuid VARCHAR(127) NOT NULL,
     request_type VARCHAR(63)  NOT NULL,
     -- Raw command Plist
-    command      MEDIUMTEXT   NOT NULL,
+    command      TEXT         NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- TODO  MySQL ON UPDATE CURRENT_TIMESTAMP to pgSQL trigger func
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (command_uuid),
 
-    CHECK (command_uuid != ''),
+    CHECK (command_uuid != '') ,
     CHECK (request_type != ''),
     CHECK (SUBSTRING(command FROM 1 FOR 5) = '<?xml')
 );
@@ -160,17 +170,19 @@ CREATE TABLE commands (
  * means we lose insight into when NotNows happen once a command is
  * Acknowledged.
  */
-CREATE TABLE command_results (
-    id           VARCHAR(255) NOT NULL,
-    command_uuid VARCHAR(127) NOT NULL,
-    status       VARCHAR(31)  NOT NULL,
-    result       MEDIUMTEXT   NOT NULL,
+CREATE TABLE command_results
+(
+    id            VARCHAR(255) NOT NULL,
+    command_uuid  VARCHAR(127) NOT NULL,
+    status        VARCHAR(31)  NOT NULL,
+    result        TEXT         NOT NULL,
 
     not_now_at    TIMESTAMP NULL,
-    not_now_tally INTEGER NOT NULL DEFAULT 0,
+    not_now_tally INTEGER      NOT NULL DEFAULT 0,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at    TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+    -- TODO trigger on change
+    updated_at    TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id, command_uuid),
 
@@ -184,21 +196,24 @@ CREATE TABLE command_results (
 
     -- considering not enforcing these CHECKs to make sure we always
     -- capture results in the case they're malformed.
-    CHECK (status != ''),
-    INDEX (status),
+    CHECK (status != ''
+) ,
     CHECK (SUBSTRING(result FROM 1 FOR 5) = '<?xml')
 );
+CREATE INDEX idx_status ON command_results (status);
 
 
-CREATE TABLE enrollment_queue (
+CREATE TABLE enrollment_queue
+(
     id           VARCHAR(255) NOT NULL,
     command_uuid VARCHAR(127) NOT NULL,
 
-    active   BOOLEAN NOT NULL DEFAULT 1,
-    priority TINYINT NOT NULL DEFAULT 0,
+    active       BOOLEAN      NOT NULL DEFAULT TRUE,
+    priority     SMALLINT     NOT NULL DEFAULT 0,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at   TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+    -- TODO trigger on update
+    updated_at   TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id, command_uuid),
 
@@ -216,36 +231,35 @@ CREATE TABLE enrollment_queue (
  * those that have received no result yet) will have a status of NULL
  * (due to the LEFT JOIN against results).
  */
-CREATE OR REPLACE VIEW view_queue AS
-SELECT
-    q.id,
-    q.created_at,
-    q.active,
-    q.priority,
-    c.command_uuid,
-    c.request_type,
-    c.command,
-    r.updated_at AS result_updated_at,
-    r.status,
-    r.result
-FROM
-    enrollment_queue AS q
+CREATE
+OR REPLACE VIEW view_queue AS
+SELECT q.id,
+       q.created_at,
+       q.active,
+       q.priority,
+       c.command_uuid,
+       c.request_type,
+       c.command,
+       r.updated_at AS result_updated_at,
+       r.status,
+       r.result
+FROM enrollment_queue AS q
 
-        INNER JOIN commands AS c
-        ON q.command_uuid = c.command_uuid
+         INNER JOIN commands AS c
+                    ON q.command_uuid = c.command_uuid
 
-        LEFT JOIN command_results r
-        ON r.command_uuid = q.command_uuid AND r.id = q.id
-ORDER BY
-    q.priority DESC,
-    q.created_at;
+         LEFT JOIN command_results r
+                   ON r.command_uuid = q.command_uuid AND r.id = q.id
+ORDER BY q.priority DESC,
+         q.created_at;
 
 
-CREATE TABLE push_certs (
-    topic VARCHAR(255) NOT NULL,
+CREATE TABLE push_certs
+(
+    topic       VARCHAR(255) NOT NULL,
 
-    cert_pem TEXT NOT NULL,
-    key_pem  TEXT NOT NULL,
+    cert_pem    TEXT         NOT NULL,
+    key_pem     TEXT         NOT NULL,
 
     /* stale_token is a simple value that coordinates push certificates
      * across the SQL backend. The push service checks this value
@@ -253,28 +267,33 @@ CREATE TABLE push_certs (
      * every time a push cert is updated (i.e. renwals) and so all
      * push services using this table will know the certificate has
      * changed and reload it. This is managed by the MySQL backend. */
-    stale_token INTEGER NOT NULL,
+    stale_token INTEGER      NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- TODO trigger on update
+    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (topic),
-    CHECK (topic != ''),
+    CHECK (topic != ''
+) ,
 
     CHECK (SUBSTRING(cert_pem FROM 1 FOR 27) = '-----BEGIN CERTIFICATE-----'),
     CHECK (SUBSTRING(key_pem  FROM 1 FOR  5) = '-----')
 );
 
 
-CREATE TABLE cert_auth_associations (
-    id     VARCHAR(255) NOT NULL,
-    sha256 CHAR(64)     NOT NULL,
+CREATE TABLE cert_auth_associations
+(
+    id         VARCHAR(255) NOT NULL,
+    sha256     CHAR(64)     NOT NULL,
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    --TODO trigger on update
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id, sha256),
 
-    CHECK (id != ''),
+    CHECK (id != ''
+) ,
     CHECK (sha256 != '')
 );
