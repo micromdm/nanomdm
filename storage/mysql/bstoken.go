be1171ede983fe1dd81a7d4b4f675335d352019f
Calvin Lee <pounce@meta.com>
2023-01-10 13:04:38 -0800

    Fix error on MySQL backend when bsToken null
    This commit fixes an error when the bootstrap token for a device is NULL. Apple
    specifies[0] the behaviour of `GetBootstrapToken` to return an empty body when
    there is no bootstrap token for the device. This behaviour has been conformed
    to.
    
    [0]: https://developer.apple.com/documentation/devicemanagement/get_bootstrap_token

--- storage/mysql/bstoken.go
+++ storage/mysql/bstoken.go
@@ -1,6 +1,8 @@
 package mysql
 
 import (
+	"database/sql"
+
 	"github.com/micromdm/nanomdm/mdm"
 )
 
@@ -18,17 +20,17 @@ func (s *MySQLStorage) StoreBootstrapToken(r *mdm.Request, msg *mdm.SetBootstrap
 }
 
 func (s *MySQLStorage) RetrieveBootstrapToken(r *mdm.Request, _ *mdm.GetBootstrapToken) (*mdm.BootstrapToken, error) {
-	var tokenB64 string
+	var tokenB64 sql.NullString
 	err := s.db.QueryRowContext(
 		r.Context,
 		`SELECT bootstrap_token_b64 FROM devices WHERE id = ?;`,
 		r.ID,
 	).Scan(&tokenB64)
-	if err != nil {
+	if err != nil || !tokenB64.Valid {
 		return nil, err
 	}
 	bsToken := new(mdm.BootstrapToken)
-	err = bsToken.SetTokenString(tokenB64)
+	err = bsToken.SetTokenString(tokenB64.String)
 	if err == nil {
 		err = s.updateLastSeen(r)
 	}
