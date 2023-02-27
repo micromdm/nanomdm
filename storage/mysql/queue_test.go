2f6946319be7b44ab23070c5bdf94f02c47adeda
Calvin Lee <pounce@meta.com>
2023-02-27 09:28:45 -0800

    Add tests for BootstrapToken changes
    This commit refactors the MySQL tests, and puts some common code into
    the storage test library. Instead of depending on the "github.com/google/uuid"
    library, we grab device UDIDs from our test data.
    
    The pgsql changes should also be refactored to use these changes, but I
    think that deserves another PR.

--- storage/mysql/queue_test.go
+++ storage/mysql/queue_test.go
@@ -4,83 +4,12 @@
 package mysql
 
 import (
-	"context"
-	"errors"
-	"flag"
-	"io/ioutil"
 	"testing"
 
-	"github.com/micromdm/nanomdm/mdm"
 	"github.com/micromdm/nanomdm/storage/internal/test"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-var flDSN = flag.String("dsn", "", "DSN of test MySQL instance")
-
-func loadAuthMsg() (*mdm.Authenticate, error) {
-	b, err := ioutil.ReadFile("../../mdm/testdata/Authenticate.2.plist")
-	if err != nil {
-		return nil, err
-	}
-	r, err := mdm.DecodeCheckin(b)
-	if err != nil {
-		return nil, err
-	}
-	a, ok := r.(*mdm.Authenticate)
-	if !ok {
-		return nil, errors.New("not an Authenticate message")
-	}
-	return a, nil
-}
-
-func loadTokenMsg() (*mdm.TokenUpdate, error) {
-	b, err := ioutil.ReadFile("../../mdm/testdata/TokenUpdate.2.plist")
-	if err != nil {
-		return nil, err
-	}
-	r, err := mdm.DecodeCheckin(b)
-	if err != nil {
-		return nil, err
-	}
-	a, ok := r.(*mdm.TokenUpdate)
-	if !ok {
-		return nil, errors.New("not a TokenUpdate message")
-	}
-	return a, nil
-}
-
-const deviceUDID = "66ADE930-5FDF-5EC4-8429-15640684C489"
-
-func newMdmReq() *mdm.Request {
-	return &mdm.Request{
-		Context: context.Background(),
-		EnrollID: &mdm.EnrollID{
-			Type: mdm.Device,
-			ID:   deviceUDID,
-		},
-	}
-}
-
-func enrollTestDevice(storage *MySQLStorage) error {
-	authMsg, err := loadAuthMsg()
-	if err != nil {
-		return err
-	}
-	err = storage.StoreAuthenticate(newMdmReq(), authMsg)
-	if err != nil {
-		return err
-	}
-	tokenMsg, err := loadTokenMsg()
-	if err != nil {
-		return err
-	}
-	err = storage.StoreTokenUpdate(newMdmReq(), tokenMsg)
-	if err != nil {
-		return err
-	}
-	return nil
-}
 
 func TestQueue(t *testing.T) {
 	if *flDSN == "" {
@@ -92,13 +21,13 @@ func TestQueue(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	err = enrollTestDevice(storage)
+	d, err := enrollTestDevice(storage)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	t.Run("WithDeleteCommands()", func(t *testing.T) {
-		test.TestQueue(t, deviceUDID, storage)
+		test.TestQueue(t, d.UDID, storage)
 	})
 
 	storage, err = New(WithDSN(*flDSN))
@@ -107,6 +36,6 @@ func TestQueue(t *testing.T) {
 	}
 
 	t.Run("normal", func(t *testing.T) {
-		test.TestQueue(t, deviceUDID, storage)
+		test.TestQueue(t, d.UDID, storage)
 	})
 }
