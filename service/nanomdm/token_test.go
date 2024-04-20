1b07afa742999410c108d1567a15d20f91207a4d
Jesse Peterson <jesse.c.peterson@gmail.com>
2024-04-20 15:52:50 -0700

    add token muxer and static token helper

---
+++ service/nanomdm/token_test.go
@@ -0,0 +1,28 @@
+package nanomdm
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/micromdm/nanomdm/mdm"
+)
+
+func TestToken(t *testing.T) {
+	m := NewTokenServiceTypeMux()
+	inTok := []byte("hello")
+	m.Handle("com.apple.maid", NewStaticToken(inTok))
+	inMDMGetToken := &mdm.GetToken{TokenServiceType: "com.apple.maid"}
+	outTok, err := m.GetToken(nil, inMDMGetToken)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(inTok, outTok.TokenData) {
+		t.Error("input and output not equal")
+	}
+	// invalid type
+	inMDMGetToken = &mdm.GetToken{TokenServiceType: "com.apple.does-not-exist"}
+	_, err = m.GetToken(nil, inMDMGetToken)
+	if err == nil {
+		t.Fatal("should be an error")
+	}
+}
