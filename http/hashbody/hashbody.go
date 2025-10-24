c130a86ecc64233274cdf936e66d39f0d6facc69
Jesse Peterson <jesse.c.peterson@gmail.com>
2025-10-23 21:56:56 -0700

    remove body functions moved to nanolib

--- http/hashbody/hashbody.go
+++ http/hashbody/hashbody.go
@@ -1,14 +1,13 @@
 package hashbody
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
-	"errors"
 	"fmt"
 	"hash"
-	"io"
 	"net/http"
+
+	libhttp "github.com/micromdm/nanolib/http"
 )
 
 // SetBodyHashHeader reads the body of req and writes it to hasher to encode an HTTP header.
@@ -27,8 +26,7 @@ func SetBodyHashHeader(req *http.Request, header string, hasher hash.Hash, encod
 		encoder = hex.EncodeToString
 	}
 
-	err := GetAndReplaceBody(req, hasher)
-	if err != nil {
+	if err := libhttp.GetAndReplaceBody(req, hasher); err != nil {
 		return "", fmt.Errorf("getting body: %w", err)
 	}
 
@@ -37,54 +35,4 @@ func SetBodyHashHeader(req *http.Request, header string, hasher hash.Hash, encod
 	req.Header.Set(header, encoded)
 
 	return encoded, nil
-}
-
-// GetAndReplaceBody returns the body of req as a bytes slice.
-// If needed the body is replaced with a byte buffer for re-use.
-func GetAndReplaceBodyBytes(req *http.Request) ([]byte, error) {
-	buf := new(bytes.Buffer)
-	err := GetAndReplaceBody(req, buf)
-	return buf.Bytes(), err
-}
-
-// GetAndReplaceBody writes the body of req to w.
-// If needed the body is replaced with a byte buffer for re-use.
-func GetAndReplaceBody(req *http.Request, w io.Writer) error {
-	if req == nil {
-		return errors.New("nil request")
-	}
-	if req.Body == nil || w == nil {
-		return nil
-	}
-
-	var err error
-
-	body := req.Body
-	var buf *bytes.Buffer
-
-	if req.GetBody != nil {
-		// GetBody returns a copy of a the body for reading
-		// using it implies we don't need to replace the body
-		body, err = req.GetBody()
-		if err != nil {
-			return fmt.Errorf("getting body: %w", err)
-		} else if body == nil {
-			return nil
-		}
-	} else {
-		buf = new(bytes.Buffer)
-		w = io.MultiWriter(buf, w)
-	}
-
-	_, err = io.Copy(w, body)
-	if err != nil {
-		return fmt.Errorf("copying body: %w", err)
-	}
-	defer body.Close()
-
-	if buf != nil {
-		req.Body = io.NopCloser(buf)
-	}
-
-	return nil
 }
